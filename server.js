b8daa291ae2be39bef44592a88d7d25503e0ec28
melakutena <88937228+melakutena@users.noreply.github.com>
2023-12-16 14:47:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var server = http.createServer(function (req, res) {   //create web server
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         
         // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.write('<html><body><p>This is my home Page.</p></body></html>');
         res.end();
     
     }
     else if (req.url == "/student") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.write('<html><body><p>This is my student Page.</p></body></html>');
         res.end();
     
     }
